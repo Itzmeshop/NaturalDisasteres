fa362dde1ca7d9195f234e94bc5ff2e1c196656f
Naturald1sasterS <matveevgresed@gmail.com>
2026-07-03 00:03:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,35 @@
+import { defineConfig } from "vite";
 
+export default defineConfig({
+
+    server: {
+
+        host: "0.0.0.0",
+
+        port: 5173,
+
+        open: true
+
+    },
+
+    preview: {
+
+        host: "0.0.0.0",
+
+        port: 4173
+
+    },
+
+    build: {
+
+        outDir: "dist",
+
+        assetsDir: "assets",
+
+        sourcemap: false,
+
+        emptyOutDir: true
+
+    }
+
+});
