68d7db776831dbda0652873433acca45860d0140
Naturald1sasterS <matveevgresed@gmail.com>
2026-07-03 00:46:55 +0300

    Update EventSystem.js

--- src/utils/EventSystem.js
+++ src/utils/EventSystem.js
@@ -1,1 +1,53 @@
+export default class EventSystem {
 
+    constructor() {
+
+        this.events = {};
+    }
+
+    // =========================
+    // ПОДПИСКА
+    // =========================
+
+    on(event, callback) {
+
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+
+        this.events[event].push(callback);
+    }
+
+    // =========================
+    // УДАЛЕНИЕ ПОДПИСКИ
+    // =========================
+
+    off(event, callback) {
+
+        if (!this.events[event]) return;
+
+        this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
+
+    // =========================
+    // ВЫЗОВ СОБЫТИЙ
+    // =========================
+
+    emit(event, data) {
+
+        if (!this.events[event]) return;
+
+        for (const cb of this.events[event]) {
+            cb(data);
+        }
+    }
+
+    // =========================
+    // ОЧИСТКА
+    // =========================
+
+    clear() {
+
+        this.events = {};
+    }
+}
