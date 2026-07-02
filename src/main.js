feb9e06f78b8d01e5df4e482036e2746e3e36395
Naturald1sasterS <matveevgresed@gmail.com>
2026-07-03 00:07:00 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
+import Phaser from "phaser";
 
+// Сцены (мы их создадим дальше)
+import BootScene from "./scenes/BootScene.js";
+import PreloadScene from "./scenes/PreloadScene.js";
+import MenuScene from "./scenes/MenuScene.js";
+import GameScene from "./scenes/GameScene.js";
+
+const config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: "game-container",
+    backgroundColor: "#0b1d1a",
+
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+            gravity: { y: 0 }
+        }
+    },
+
+    scene: [
+        BootScene,
+        PreloadScene,
+        MenuScene,
+        GameScene
+    ],
+
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+};
+
+// Запуск игры
+new Phaser.Game(config);
