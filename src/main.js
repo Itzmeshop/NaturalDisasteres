8212f866044642893f6fda7427b2e93749afc6a6
Naturald1sasterS <matveevgresed@gmail.com>
2026-07-03 16:38:33 +0300

    Simplify Phaser game configuration

--- src/main.js
+++ src/main.js
@@ -1,38 +1,11 @@
 import Phaser from "phaser";
-
-// Сцены (мы их создадим дальше)
-import BootScene from "./scenes/BootScene";       
-import PreloadScene from "./scenes/PreloadScene.js";
-import MenuScene from "./scenes/MenuScene.js";
 import GameScene from "./scenes/GameScene.js";
 
 const config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    parent: "game-container",
-    backgroundColor: "#0b1d1a",
-
-    physics: {
-        default: "arcade",
-        arcade: {
-            debug: false,
-            gravity: { y: 0 }
-        }
-    },
-
-    scene: [
-        BootScene,
-        PreloadScene,
-        MenuScene,
-        GameScene
-    ],
-
-    scale: {
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [GameScene]
 };
 
-// Запуск игры
 new Phaser.Game(config);
